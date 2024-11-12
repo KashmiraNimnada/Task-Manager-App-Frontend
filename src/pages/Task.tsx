f700d0f9d830bec964f3c9be6256a58308a64d2e
KashmiraNimnada <kashmiranimnada98@gmail.com>
2024-11-12 15:28:50 +0530

    Checkout button to show the task status and update task completed

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 function Task() {
 
     const [tasks,setTasks] = useState<TaskType[]>([]);
+    const [completed,isCompleted] = useState<boolean>(false);
 
     async function loadTasks() {
         try {
@@ -14,6 +15,49 @@ function Task() {
         } catch (error) {
             console.log(error)
         }
+    }
+
+    async function updateTask(id:number) {
+        if(completed) {
+            const req1 = {
+                "name" : "Java assignment 3",
+                "description" : "This assignment contains about Arrays",
+                "duedate" : "2024-11-30",
+                "status" : "COMPLETED",
+                "user_id" : 3
+            }
+
+            try {
+                const response1 = await axios.put(`http://localhost:8080/tasks/${id}`,req1)
+                console.log(response1.data);
+                loadTasks()
+            } catch (error) {
+                console.log(error)
+            }
+
+        }else {
+            const req2 = {
+                "name" : "Java assignment 3",
+                "description" : "This assignment contains about Arrays",
+                "duedate" : "2024-11-30",
+                "status" : "PENDING",
+                "user_id" : 3
+            }
+
+            try {
+                const response2 = await axios.put(`http://localhost:8080/tasks/${id}`,req2)
+                console.log(response2.data)
+                loadTasks()
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
+    }
+
+    function setStatus(id:number) {
+        isCompleted(!completed);
+        updateTask(id);
     }
 
     useEffect(function(){
@@ -39,15 +83,31 @@ function Task() {
                     </thead>
                     <tbody>
                         {tasks && tasks.map(function(task:TaskType){
-                            return (
-                                <tr>
-                                    <td className="px-6 py-2 text-center">{task.name}</td>
-                                    <td className="px-6 py-2 text-center">{task.description}</td>
-                                    <td className="px-6 py-2 text-center">{task.duedate}</td>
-                                    <td className="px-6 py-2 text-center"></td>
-                                    <td className="px-6 py-2 text-center"></td>
-                                </tr>
-                            )
+                            if(task.status=="COMPLETED") {
+                                return (
+                                    <tr key={task.id}>
+                                        <td className="px-6 py-2 line-through">{task.name}</td>
+                                        <td className="px-6 py-2 line-through">{task.description}</td>
+                                        <td className="px-6 py-2 line-through">{task.duedate}</td>
+                                        <td className="px-6 py-2">
+                                            <button onClick={() => setStatus(task.id)} className="bg-gradient-to-r text-sm from-cyan-500 to-blue-500 rounded-md p-2 hover:scale-105 duration-300 text-white">{task.status}</button>
+                                        </td>
+                                        <td className="px-6 py-2 text-center"></td>
+                                    </tr>
+                                )
+                            } else {
+                                return (
+                                    <tr key={task.id}>
+                                        <td className="px-6 py-2">{task.name}</td>
+                                        <td className="px-6 py-2">{task.description}</td>
+                                        <td className="px-6 py-2">{task.duedate}</td>
+                                        <td className="px-6 py-2">
+                                            <button onClick={() => setStatus(task.id)} className="bg-gradient-to-r text-sm from-cyan-500 to-blue-500 rounded-md p-2 hover:scale-105 duration-300 text-white">{task.status}</button>
+                                        </td>
+                                        <td className="px-6 py-2 text-center"></td>
+                                    </tr>
+                                )
+                            }
                         })}
                     </tbody>
                 </table>
